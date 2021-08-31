507827cb0f7f350fa865c63e6cb25a0419c7dc5e
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-08-31 15:27:20 +0300

    remove and change the icons

--- src/pages/exchange/add/[[...tokens]].tsx
+++ src/pages/exchange/add/[[...tokens]].tsx
@@ -315,7 +315,7 @@ export default function Add() {
   return (
     <>
       <Head>
-        <title>Add Liquidity | Sushi</title>
+        <title>Add Liquidity | Summit</title>
         <meta
           key="description"
           name="description"
